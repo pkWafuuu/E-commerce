79fe7607fec79ceddc6cfc511993a371a136968f
Phil <pk.oaferina@gmail.com>
2023-04-13 16:50:06 +0400

    printed all books using JS

--- books.js
+++ books.js
@@ -1,3 +1,38 @@
+function renderBooks() {
+	const booksWrapper = document.querySelector('.books');
+
+	const books = getBooks();
+
+	const booksHtml = books.map(book => {
+		return `<div class="book">
+		<figure class="book__img--wrapper">
+			<img class="book__img" src="${book.url}" alt="">
+		</figure>
+		<div class="book__title">
+			${book.title}
+		</div>
+		<div class="book__ratings">
+			<i class="fas fa-star"></i>
+			<i class="fas fa-star"></i>
+			<i class="fas fa-star"></i>
+			<i class="fas fa-star"></i>
+			<i class="fas fa-star-half-alt"></i>
+		</div>
+		<div class="book__price">
+			<span class="book__price--normal">$${book.originalPrice}</span> $${book.salePrice}
+		</div>
+	</div>`;
+	}).join('');
+
+
+
+	booksWrapper.innerHTML = booksHtml;
+	
+}
+
+setTimeout(() => {
+	renderBooks();
+});
 
 
 // FAKE DATA
@@ -6,7 +41,7 @@ function getBooks() {
     {
       id: 1,
       title: "Crack the Coding Interview",
-                url: "assets/crack the coding interview.png",
+      url: "assets/crack the coding interview.png",
       originalPrice: 49.95,
       salePrice: 14.95,
       rating: 4.5,
